9ae31feb1e987f1aa449b8f9c6782a5b2e2c45a4
Silnyagin Yury <sgloin@yandex.ru>
2017-09-20 15:29:22 +0300

    Img loading and displaying

--- CVProject2/Form1.Designer.cs
+++ CVProject2/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goodExamplesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.badExamplesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goodExamplesToolStripMenuItem,
+            this.badExamplesToolStripMenuItem});
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            // 
+            // goodExamplesToolStripMenuItem
+            // 
+            this.goodExamplesToolStripMenuItem.Name = "goodExamplesToolStripMenuItem";
+            this.goodExamplesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.goodExamplesToolStripMenuItem.Text = "Good Examples";
+            this.goodExamplesToolStripMenuItem.Click += new System.EventHandler(this.goodExamplesToolStripMenuItem_Click);
+            // 
+            // badExamplesToolStripMenuItem
+            // 
+            this.badExamplesToolStripMenuItem.Name = "badExamplesToolStripMenuItem";
+            this.badExamplesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.badExamplesToolStripMenuItem.Text = "Bad Examples";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(752, 623);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hough Demo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goodExamplesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem badExamplesToolStripMenuItem;
     }
 }
 
--- CVProject2/Form1.cs
+++ CVProject2/Form1.cs
@@ -8,13 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using System.Collections;
+
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Blob;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.UserInterface;
 namespace CVProject2
 {
     public partial class Form1 : Form
     {
+        String gpath = System.IO.Path.GetFullPath(@"..\..\Good");
+        String bpath = System.IO.Path.GetFullPath(@"..\..\Bad");
+
         public Form1()
         {
             InitializeComponent();
+        }              
+        
+
+        private void goodExamplesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] pics = Directory.GetFiles(gpath);
+
+            int n = 0, m = 0;
+            foreach (string p in pics)
+            {
+                var img = Image.FromFile(p);
+                
+                img = (Image) new Bitmap(img, new System.Drawing.Size(150,100));                
+
+                var picture = new PictureBox
+                {
+                    Name = "pictureBox"+n.ToString(),
+                    Size = new System.Drawing.Size(150, 100),
+                    Location = new System.Drawing.Point(150*n, 25+100*m),
+                    Image = img
+                };
+                this.Controls.Add(picture);
+
+                n++;
+                if(n%5==0 && n!=0) { m++; n = 0; }
+                    
+            }
         }
     }
 }
